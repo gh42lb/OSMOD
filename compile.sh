fd1af76ec1182d04efb1baba35e055f0426440e0
Lawrence Byng <92827417+gh42lb@users.noreply.github.com>
2025-04-27 13:08:03 -1000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,5 @@
 
-
-#gcc -Wall -o mytest mytest.c -lm -lc -lliquid
-#gcc -shared -o mytest.dll mytest.o
-
-
-
+#modify .so name for your platform. name chosen must match name that python detects for platform (see debug output for reference) 
 gcc -c osmod_c_code.c
 gcc -shared -o lb28_compiled_linux_raspberrypi.so osmod_c_code.o
 
